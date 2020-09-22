374c9849d615dedb3739b12bc58c803f3b9d5a09
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-09-22 21:14:33 +1000

    improved shell script

--- AWS_Lambda/scrape.sh
+++ AWS_Lambda/scrape.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-curl --silent https://www.brainyquote.com/authors/kanye-west-quotes | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' > test_kanye_quoutes.txt
-curl --silent https://www.brainyquote.com/authors/kanye-west-quotes_2 | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' >> test_kanye_quoutes.txt
-curl --silent https://www.brainyquote.com/authors/kanye-west-quotes_3 | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' >> test_kanye_quoutes.txt
+curl --silent https://www.brainyquote.com/authors/kanye-west-quotes | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' > AWS_Lambda/kanye_quoutes.txt
+curl --silent https://www.brainyquote.com/authors/kanye-west-quotes_2 | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' >> AWS_Lambda/kanye_quoutes.txt
+curl --silent https://www.brainyquote.com/authors/kanye-west-quotes_3 | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' >> AWS_Lambda/kanye_quoutes.txt
+
+# Cant find sed command to replace (').
