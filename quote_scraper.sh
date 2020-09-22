5f6126729cf581847939d659ee1e43edd6485e9e
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-09-22 21:18:07 +1000

    added comments

--- quote_scraper.sh
+++ quote_scraper.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Used this to scrape kanye quotes from websites
 
 curl --silent https://www.brainyquote.com/authors/kanye-west-quotes | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' > AWS_Lambda/kanye_quoutes.txt
 curl --silent https://www.brainyquote.com/authors/kanye-west-quotes_2 | grep 'view quote' | sed 's/^.*quote">//' | sed 's/<.*$//' >> AWS_Lambda/kanye_quoutes.txt
