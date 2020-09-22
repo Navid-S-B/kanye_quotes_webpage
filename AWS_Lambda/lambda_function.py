b43a9bc292e46d09e6babc49a09eb90a5ec1f297
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2020-09-22 12:16:52 +1000

    added lambda code

---
+++ AWS_Lambda/lambda_function.py
@@ -0,0 +1,37 @@
+import json
+from random import randint
+
+def get_quote():
+    
+    # Quote to send
+    quote_to_send = ""
+    
+    # Send error when file cannot be opened.
+    try:
+        f = open("kanye_quotes.tt", "r")
+        quotes = f.readlines()
+        quote_to_send = quotes[randint(0,len(quotes))]
+        quote_to_send = quote_to_send.rstrip()
+        f.close()
+    except:
+        quote_to_send = "Internal Server Error"
+
+    return quote_to_send
+
+
+def lambda_handler(event, context):
+    
+    send_quote = get_quote()
+
+    # Add indicator to trigger error handling on webpage.
+    if (send_quote == "Internal Server Error"):
+        success_fail = 500
+
+    return {
+        'statusCode': success_fail,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': json.dumps(get_quote())
+    }
